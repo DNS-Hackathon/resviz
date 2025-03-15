84fae7a0df780a8f5a9cb066e63025115ddce943
lol <26503572+TheNerdyHamster@users.noreply.github.com>
2025-03-15 18:25:58 +0100

    Feat: Searchbar and header

---
+++ frontend/src/components/SearchBar.vue
@@ -0,0 +1,54 @@
+<script setup>
+import { ref, defineEmits } from "vue";
+
+const searchQuery = ref("");
+const emit = defineEmits(["search"]);
+
+const handleSearch = () => {
+  emit("search", searchQuery.value);
+};
+
+</script>
+
+<template>
+  <div class="search-bar">
+    <input
+      v-model="searchQuery"
+      placeholder="Domain to visualize..."
+      type="text"
+    />
+    <button @click="handleSearch">Visualise</button>
+  </div>
+</template>
+
+<style scoped>
+.search-bar {
+  max-wdith: 10%;
+  display: flex;
+  gap: 10px;
+  margin-top: 20px;
+  margin-bottom: 10px;
+}
+
+input {
+  flex: 1;
+  padding: 15px;
+  background: var(--color-background);
+  color: var(--color-text);
+  border: 1px solid var(--color-text);
+  border-radius: 5px;
+}
+
+button {
+  padding: 8px 12px;
+  border: none;
+  background: var(--color-button);
+  color: white;
+  cursor: pointer;
+  border-radius: 5px;
+}
+
+button:hover {
+  background: var(--color-button-hover);
+}
+</style>
