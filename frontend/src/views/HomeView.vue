9835ee8039282192cfb1b07782d64f6cbe2578fa
lol <26503572+TheNerdyHamster@users.noreply.github.com>
2025-03-16 12:18:28 +0100

    Feat: ADDED API SUPPORT!

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,73 +1,14 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted } from "vue";
+import axios from 'axios';
 import MermaidChart from '@/components/MermaidChart.vue'
 import ChartNavigator from '@/components/ChartNavigator.vue'
 import SearchBar from '@/components/SearchBar.vue'
 
 const step = ref(0);
 
-const charts = [
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-click a.root-servers.net #
-  `,
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-c.gtld-servers.net@{ shape: rect, label: "c.gtld-servers.net"}
-a.root-servers.net --> c.gtld-servers.net
-  `,
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-c.gtld-servers.net@{ shape: rect, label: "c.gtld-servers.net"}
-ns2.dns-oarc.net@{ shape: rect, label: "ns2.dns-oarc.net"}
-a.root-servers.net --> c.gtld-servers.net
-c.gtld-servers.net --> ns2.dns-oarc.net
+const charts = ref(null);
 
-  `,
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-c.gtld-servers.net@{ shape: rect, label: "c.gtld-servers.net"}
-ns2.dns-oarc.net@{ shape: rect, label: "ns2.dns-oarc.net"}
-ns.dev.dns-oarc.net@{ shape: rect, label: "ns.dev.dns-oarc.net"}
-
-
-a.root-servers.net --> c.gtld-servers.net
-c.gtld-servers.net --> ns2.dns-oarc.net
-ns2.dns-oarc.net --> ns.dev.dns-oarc.net
-  `,
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-c.gtld-servers.net@{ shape: rect, label: "c.gtld-servers.net"}
-ns2.dns-oarc.net@{ shape: rect, label: "ns2.dns-oarc.net"}
-ns.dev.dns-oarc.net@{ shape: rect, label: "ns.dev.dns-oarc.net"}
-ns.cmdns.dev.dns-oarc.net@{ shape: rect, label: "ns.cmdns.dev.dns-oarc.net"}
-
-
-a.root-servers.net --> c.gtld-servers.net
-c.gtld-servers.net --> ns2.dns-oarc.net
-ns2.dns-oarc.net --> ns.dev.dns-oarc.net
-ns.dev.dns-oarc.net --> ns.cmdns.dev.dns-oarc.net
-  `,
-  `graph TD;
-a.root-servers.net@{ shape: rect, label: "a.root-servers.net"}
-c.gtld-servers.net@{ shape: rect, label: "c.gtld-servers.net"}
-ns2.dns-oarc.net@{ shape: rect, label: "ns2.dns-oarc.net"}
-ns.dev.dns-oarc.net@{ shape: rect, label: "ns.dev.dns-oarc.net"}
-ns.cmdns.dev.dns-oarc.net@{ shape: rect, label: "ns.cmdns.dev.dns-oarc.net"}
-domain@{ shape: rect, label: "dns-oarc.net"}
-
-
-a.root-servers.net --> c.gtld-servers.net
-c.gtld-servers.net --> ns2.dns-oarc.net
-ns2.dns-oarc.net --> ns.dev.dns-oarc.net
-ns.dev.dns-oarc.net --> ns.cmdns.dev.dns-oarc.net
-ns.cmdns.dev.dns-oarc.net --> domain
-  `,
-];
-
-const callback = () => {
-  console.log("testing")
-}
 const nextStep = () => {
   if (step.value < charts.length -1) step.value++;
 };
@@ -76,8 +17,18 @@ const prevStep = () => {
   if (step.value > 0) step.value--;
 };
 
-const handleSearch = (query) => {
-  console.log(query);
+const handleSearch = async (query) => {
+  try {
+    const response = await axios.post("http://nerdig.examples.nu/resviz", query);
+
+    console.log(response.data);
+    charts.value = [response.data];
+    if (response.status !== 200) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+  } catch (err) {
+    alert(err);
+  }
 };
 
 const handleKeydown = (event) => {
@@ -100,7 +51,9 @@ onUnmounted(() => {
 <template>
   <main>
     <SearchBar @search="handleSearch" />
-    <ChartNavigator :step="step" :maxSteps="charts.length" @next="nextStep" @prev="prevStep" />
-    <MermaidChart :step="step" :charts="charts"/>
+    <div v-if="charts">
+      <ChartNavigator :step="step" :maxSteps="charts.length" @next="nextStep" @prev="prevStep" />
+      <MermaidChart :step="step" :charts="charts"/>
+    </div>
   </main>
 </template>
